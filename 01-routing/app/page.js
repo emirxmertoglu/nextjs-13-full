88d3977cb548960275af17e24f37077e22e22557
emirxmertoglu <emir.mertoglu.97@gmail.com>
2023-03-01 00:46:10 +0300

    next link

--- 01-routing/app/page.js
+++ 01-routing/app/page.js
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Link href="/about">About</Link>
+      <br />
+      <Link href="/docs">Docs</Link>
+      <br />
+      <Link href="/docs/getting-started">Docs - Getting Started</Link>
+      <br />
+      <Link
+        target="_blank"
+        className="test"
+        href={{
+          pathname: "/docs/getting-started",
+          query: { name: "Emir", age: 25 },
+        }}
+      >
+        Docs - Getting Started (with object)
+      </Link>
+    </div>
+  );
 }
